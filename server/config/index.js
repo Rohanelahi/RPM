3bd7db59e6f0d20d2a27f4838fae90d08cdaa2f2
Rohan Elahi <rohanelahi@gmail.com>
2025-04-24 14:11:52 +0500

    heheh

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   apiUrl: process.env.API_URL || 'http://150.230.192.101:5000/api',
   port: process.env.PORT || 5000,
   database: {
-    host: process.env.DB_HOST || '150.136.153.101',
+    host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || 'rosepapermill',
     user: process.env.DB_USER || 'opc',
